56f0275715c81b5f4ce73aef2ff48e14cc8a3460
John Shahid <john.shahid@benchmarksolutions.com>
2012-04-08 11:51:32 -0400

    Use Nominatim to translate addresses to longitude and latitude.

--- parse.rb
+++ parse.rb
@@ -1,7 +1,43 @@
 #!/usr/bin/env ruby
 
 require 'csv'
+require 'net/http'
+require 'json'
+require 'cgi'
 
-CSV.foreach("../Marco/BOGOTA.csv", :headers => true, :col_sep => '|') do |csv_obj|
-  puts csv_obj.to_hash.keys.inspect
+def email_filename
+  "#{ENV['HOME']}/.email_address"
+end
+
+def _email
+  if File.exists?(email_filename)
+    email = File.readlines(email_filename).first.strip
+    { :email => email }
+  else
+    puts "Make sure you create #{email_filename} with your email in it, otherwise you won't be able to make too many requests to open street map"
+    { }
+  end
+end
+
+def email
+  @email ||= _email
+end
+
+def find address
+  params = email.merge :q => address, :format => "json", :countrycodes => "CO"
+  query_params = params.map {|k,v| CGI.escape(k.to_s)+'='+CGI.escape(v.to_s) }.join("&")
+
+  puts query_params
+
+  response = Net::HTTP.start('nominatim.openstreetmap.org', 80) do |http|
+    http.get("/search?#{query_params}")
+  end
+
+  JSON.parse(response.body)
+end
+
+first = CSV.open("../Marco/BOGOTA.csv", :headers => true, :col_sep => '|').first.to_hash
+
+first.values.each do |value|
+  puts JSON.pretty_generate(find(value))
 end
