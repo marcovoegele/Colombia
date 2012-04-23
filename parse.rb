8833bb9dead81ae23d5df30f607b066361aa0287
John Shahid <john.shahid@benchmarksolutions.com>
2012-04-23 17:33:38 -0400

    Merge the address with the BOGOTA data.

--- parse.rb
+++ parse.rb
@@ -36,8 +36,35 @@ def find address
   JSON.parse(response.body)
 end
 
-first = CSV.open("../Marco/BOGOTA.csv", :headers => true, :col_sep => '|').first.to_hash
+rows = CSV.open("../Marco/BOGOTA.csv", :headers => true, :return_headers => true, :col_sep => '|')
+clients = CSV.open("../Marco/John4.csv", :headers => true, :col_sep => ',')
 
-first.values.each do |value|
-  puts JSON.pretty_generate(find(value['Sector']))
+output_columns = rows.first.to_hash.keys << "Address"
+
+client_id_to_client_address = clients.inject({ }) do |hash, client|
+  hash[client['Client ID']] = client
+  hash
+end
+
+rows_count = 0
+missing_client_ids = []
+CSV.open("../Marco/output.csv", "wb", :col_sep => '|') do |csv|
+  csv << output_columns
+  rows.each do |row|
+    client_id = row['Cliente']
+    client = client_id_to_client_address[client_id]
+    address = "N/A"
+    if client
+      address = client['Address']
+    else
+      puts "Cannot find client with id #{client_id}" unless missing_client_ids.include?(client_id)
+      missing_client_ids << client_id
+    end
+    output_row = row.to_hash.merge({ 'Address' => address})
+    csv << output_columns.map{ |_| output_row[_] }
+    rows_count += 1
+    if rows_count % 1000 == 0
+      puts "Inserted #{rows_count} rows"
+    end
+  end
 end
