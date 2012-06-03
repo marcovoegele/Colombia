37f90de22a045e2690f78e83a7b7ec7c4c9e94e6
John Shahid <john.shahid@benchmarksolutions.com>
2012-06-02 23:41:59 -0400

    Use the activerecord models instead of parsing the csv.

--- parse.rb
+++ parse.rb
@@ -1,26 +1,11 @@
 #!/usr/bin/env ruby
 
-require 'csv'
+$:.unshift File.dirname(__FILE__)
+
 require 'net/http'
 require 'json'
 require 'cgi'
-
-if ARGV.count != 3
-  puts "Usage: #{$0} <location of BOGOTA.csv> <location of client ids csv> <location of output>"
-  exit 1
-end
-
-def bogota_location
-  ARGV[0]
-end
-
-def clients_location
-  ARGV[1]
-end
-
-def output_location
-  ARGV[2]
-end
+require 'colombia'
 
 def email_filename
   "#{ENV['HOME']}/.email_address"
@@ -53,35 +38,9 @@ def find address
   JSON.parse(response.body)
 end
 
-rows = CSV.open(bogota_location, :headers => true, :return_headers => true, :col_sep => '|')
-clients = CSV.open(clients_location, :headers => true, :col_sep => ',')
-
-output_columns = rows.first.to_hash.keys << "Address"
-
-client_id_to_client_address = clients.inject({ }) do |hash, client|
-  hash[client['Client ID']] = client
-  hash
-end
-
-rows_count = 0
-missing_client_ids = []
-CSV.open(output_location, "wb", :col_sep => '|') do |csv|
-  csv << output_columns
-  rows.each do |row|
-    client_id = row['Cliente']
-    client = client_id_to_client_address[client_id]
-    address = "N/A"
-    if client
-      address = client['Address']
-    else
-      puts "Cannot find client with id #{client_id}" unless missing_client_ids.include?(client_id)
-      missing_client_ids << client_id
-    end
-    output_row = row.to_hash.merge({ 'Address' => address})
-    csv << output_columns.map{ |_| output_row[_] }
-    rows_count += 1
-    if rows_count % 1000 == 0
-      puts "Inserted #{rows_count} rows"
-    end
-  end
+Bogota.all.each do |bogota|
+  client = bogota.client
+  address = client.address
+  osm_address = find address
+  puts osm_address
 end
